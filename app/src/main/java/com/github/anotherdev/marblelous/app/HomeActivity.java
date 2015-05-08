64868d1e06297cbe4aefb1f357c7d6e3cb501dd3
Polson <>
2015-05-08 13:00:27 +0700

    initial commit

---
+++ app/src/main/java/com/github/anotherdev/marblelous/app/HomeActivity.java
@@ -0,0 +1,11 @@
+package com.github.anotherdev.marblelous.app;
+
+import android.os.Bundle;
+
+public class HomeActivity extends MActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/github/anotherdev/marblelous/app/MActivity.java
@@ -0,0 +1,12 @@
+package com.github.anotherdev.marblelous.app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
